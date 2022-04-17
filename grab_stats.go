2519fa59fa3be6206ed590dc937972e33d3cd381
Michael Rivera <soymrmike@gmail.com>
2022-04-16 19:51:06 -0700

    Can parse Legion stats sub category.

--- grab_stats.go
+++ grab_stats.go
@@ -23,8 +23,37 @@ type CharacterResponse struct {
 	Name string `json:"name"`	
 }
 
+type SubCatStatisticsResponse struct {
+	Id    int     `json:"id"`
+	Name  string  `json:"name"`
+	Quantity int  `json:"quantity"`
+}
+
+type SubCategoryResponse struct {
+	Id          int                        `json:"id"`
+	Name        string                     `json:"name"`
+	Statistics  []SubCatStatisticsResponse `json:"statistics"`
+}
+
+type CategoryResponse struct {
+	Id            int                   `json:"id"`
+	Name          string                `json:"name"`
+	SubCategories []SubCategoryResponse `json:"sub_categories"`
+}
+
 type StatsResponse struct {
-	Character  CharacterResponse `json:"character"`
+	Character   CharacterResponse  `json:"character"`
+	Categories  []CategoryResponse `json:"categories"`
+}
+
+type DungeonFinishedCount struct {
+	Description string
+	Quantity int
+}
+
+type ExpansionDungeonStats struct {
+	Name  string
+	Counts []DungeonFinishedCount
 }
 
 func main() {
@@ -56,7 +85,30 @@ func main() {
 	statsResponse := StatsResponse{}
 	json.Unmarshal(responseBody, &statsResponse)
 	
-	log.Println(statsResponse.Character.Name);
+	log.Println("Name: " + statsResponse.Character.Name);
 
-	log.Println("Here we go")
+	var dungeonCategory CategoryResponse
+	for _, cat := range statsResponse.Categories {
+		if cat.Name == "Dungeons & Raids" {
+			dungeonCategory = cat
+		}
+	}
+	
+	if dungeonCategory.Name == "" {
+		log.Fatal("Didn't find dungeon category")
+	}
+
+	var stats []ExpansionDungeonStats = []ExpansionDungeonStats{}
+	for _, subCat := range dungeonCategory.SubCategories {
+		curStats := ExpansionDungeonStats {
+			Name: subCat.Name,
+		}
+		stats = append(stats, curStats)
+	}
+
+	for _, cur := range stats {
+		log.Println(cur.Name)
+	}
+	
+	log.Println("Done")
 }
